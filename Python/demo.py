811162942254570e1a598d8f8799c48c4dea4cf3
Pranjal Joshi <joshi.pranjal5@gmail.com>
2016-07-14 17:48:46 +0530

    Pixel area is now in terms of Kilometers.

--- Python/demo.py
+++ Python/demo.py
@@ -11,13 +11,18 @@ import os
 from time import sleep
 import argparse
 
-####### REQUIRED FUNCTIONS
+####### REQUIRED FUNCTIONS & CONSTANTS
 
 def checkPath(path):
     if os.path.exists(path):
         return True
     else:
         return False
+
+radarDiameter = 250 #kms
+numberOfPixelsOnDiameter = 711
+areaOfEachPixel = (float(radarDiameter) / numberOfPixelsOnDiameter)
+areaOfEachPixel = float(areaOfEachPixel) * areaOfEachPixel     # assuming pixel shape is square and A(sqr)=side^2
 
 ###### MAIN PROGRAM BEGINS FROM HERE!!
 
@@ -75,7 +80,7 @@ cyan_mask = cv2.inRange(hsv, cyan_lower, cyan_upper)
 cyan_res = cv2.bitwise_and(img, img, mask= cyan_mask)
 pix = cv2.countNonZero(cyan_mask)
 totalPix = cv2.countNonZero(grayimg);
-pixelArea = "Pixel area of Cyan: \t\t" + str(float(pix)/totalPix*100.0) + "% [" + str(pix) + "/"+ str(totalPix) + " pix/image]"
+pixelArea = "Pixel area of Cyan: \t\t" + str(float(areaOfEachPixel)*float(pix)) + " " + " sqr Km. [" + str(pix) + "/"+ str(totalPix) + " pix/img]"
 print pixelArea
 cyan_blur = cv2.medianBlur(cyan_res,3)    #ODD values only for median filter
 cv2.putText(img,'Original',(70,50), font, 1, (255,255,255), 2, cv2.LINE_AA)
@@ -85,6 +90,7 @@ cv2.putText(cyan_mask,'CyanMask',(50,50), font, 1, (255,255,255), 2, cv2.LINE_AA
 cv2.putText(cyan_res,'CyanProcessed',(50,50), font, 1, (255,255,255), 2, cv2.LINE_AA)
 #cv2.imshow('res',cyan_res)
 cv2.putText(cyan_blur,'CyanFiltered',(50,50), font, 1, (255,255,255), 2, cv2.LINE_AA)
+cv2.putText(cyan_res, "Area: "+str(float(areaOfEachPixel)*float(pix))+" sqr Km.", (50,100),cv2.FONT_HERSHEY_SIMPLEX, 0.85, (255,255,255), 2)
 #cv2.imshow('blur',cyan_blur)
 savePath = str('/root/iitm/cv_out/'+path+'cyan.png')
 cv2.imwrite(savePath,cyan_res)
@@ -97,7 +103,7 @@ yellow_mask = cv2.inRange(hsv, yellow_lower, yellow_upper)
 yellow_res = cv2.bitwise_and(img, img, mask= yellow_mask)
 pix = cv2.countNonZero(yellow_mask)
 totalPix = cv2.countNonZero(grayimg);
-pixelArea = "Pixel area of yellow: \t\t" + str(float(pix)/totalPix*100.0) + "% [" + str(pix) + "/"+ str(totalPix) + " pix/image]"
+pixelArea = "Pixel area of yellow: \t\t" + str(float(areaOfEachPixel)*float(pix)) + " " + " sqr Km. [" + str(pix) + "/"+ str(totalPix) + " pix/img]"
 print pixelArea
 yellow_blur = cv2.medianBlur(yellow_res,3)    #ODD values only for median filter
 cv2.putText(img,'Original',(70,50), font, 1, (255,255,255), 2, cv2.LINE_AA)
@@ -105,6 +111,7 @@ cv2.putText(img,'Original',(70,50), font, 1, (255,255,255), 2, cv2.LINE_AA)
 cv2.putText(yellow_mask,'yellowMask',(50,50), font, 1, (255,255,255), 2, cv2.LINE_AA)
 #cv2.imshow('mask',yellow_mask)
 cv2.putText(yellow_res,'yellowProcessed',(50,50), font, 1, (255,255,255), 2, cv2.LINE_AA)
+cv2.putText(yellow_res, "Area: "+str(float(areaOfEachPixel)*float(pix))+" sqr Km.", (50,100),cv2.FONT_HERSHEY_SIMPLEX, 0.85, (255,255,255), 2)
 #cv2.imshow('res',yellow_res)
 cv2.putText(yellow_blur,'yellowFiltered',(50,50), font, 1, (255,255,255), 2, cv2.LINE_AA)
 #cv2.imshow('blur',yellow_blur)
@@ -119,7 +126,7 @@ red_mark_mask = cv2.inRange(hsv, red_mark_lower, red_mark_upper)
 red_mark_res = cv2.bitwise_and(img, img, mask= red_mark_mask)
 pix = cv2.countNonZero(red_mark_mask)
 totalPix = cv2.countNonZero(grayimg);
-pixelArea = "Pixel area of red_mark: \t" + str(float(pix)/totalPix*100.0) + "% [" + str(pix) + "/"+ str(totalPix) + " pix/image]"
+pixelArea = "Pixel area of red_mark: \t" + str(float(areaOfEachPixel)*float(pix)) + " " + " sqr Km. [" + str(pix) + "/"+ str(totalPix) + " pix/img]"
 print pixelArea
 red_mark_blur = cv2.medianBlur(red_mark_res,3)    #ODD values only for median filter
 cv2.putText(img,'Original',(70,50), font, 1, (255,255,255), 2, cv2.LINE_AA)
@@ -139,7 +146,7 @@ blue_mark_mask = cv2.inRange(hsv, blue_mark_lower, blue_mark_upper)
 blue_mark_res = cv2.bitwise_and(img, img, mask= blue_mark_mask)
 pix = cv2.countNonZero(blue_mark_mask)
 totalPix = cv2.countNonZero(grayimg);
-pixelArea = "Pixel area of blue_mark: \t" + str(float(pix)/totalPix*100.0) + "% [" + str(pix) + "/"+ str(totalPix) + " pix/image]"
+pixelArea = "Pixel area of blue_mark: \t" + str(float(areaOfEachPixel)*float(pix)) + " " + " sqr Km. [" + str(pix) + "/"+ str(totalPix) + " pix/img]"
 print pixelArea
 blue_mark_blur = cv2.medianBlur(blue_mark_res,3)    #ODD values only for median filter
 cv2.putText(img,'Original',(70,50), font, 1, (255,255,255), 2, cv2.LINE_AA)
@@ -159,7 +166,7 @@ sea_color_mask = cv2.inRange(hsv, sea_color_lower, sea_color_upper)
 sea_color_res = cv2.bitwise_and(img, img, mask= sea_color_mask)
 pix = cv2.countNonZero(sea_color_mask)
 totalPix = cv2.countNonZero(grayimg);
-pixelArea = "Pixel area of sea_color: \t" + str(float(pix)/totalPix*100.0) + "% [" + str(pix) + "/"+ str(totalPix) + " pix/image]"
+pixelArea = "Pixel area of sea_color: \t" + str(float(areaOfEachPixel)*float(pix)) + " " + " sqr Km. [" + str(pix) + "/"+ str(totalPix) + " pix/img]"
 print pixelArea
 sea_color_blur = cv2.medianBlur(sea_color_res,3)    #ODD values only for median filter
 cv2.putText(img,'Original',(70,50), font, 1, (255,255,255), 2, cv2.LINE_AA)
@@ -167,6 +174,7 @@ cv2.putText(img,'Original',(70,50), font, 1, (255,255,255), 2, cv2.LINE_AA)
 cv2.putText(sea_color_mask,'sea_colorMask',(50,50), font, 1, (255,255,255), 2, cv2.LINE_AA)
 #cv2.imshow('mask',sea_color_mask)
 cv2.putText(sea_color_res,'sea_colorProcessed',(50,50), font, 1, (255,255,255), 2, cv2.LINE_AA)
+cv2.putText(sea_color_res, "Area: "+str(float(areaOfEachPixel)*float(pix))+" sqr Km.", (50,100),cv2.FONT_HERSHEY_SIMPLEX, 0.85, (255,255,255), 2)
 #cv2.imshow('res',sea_color_res)
 cv2.putText(sea_color_blur,'sea_colorFiltered',(50,50), font, 1, (255,255,255), 2, cv2.LINE_AA)
 #cv2.imshow('blur',sea_color_blur)
@@ -179,7 +187,7 @@ light_green_mask = cv2.inRange(hsv, light_green_lower, light_green_upper)
 light_green_res = cv2.bitwise_and(img, img, mask= light_green_mask)
 pix = cv2.countNonZero(light_green_mask)
 totalPix = cv2.countNonZero(grayimg);
-pixelArea = "Pixel area of light_green: \t" + str(float(pix)/totalPix*100.0) + "% [" + str(pix) + "/"+ str(totalPix) + " pix/image]"
+pixelArea = "Pixel area of light_green: \t" + str(float(areaOfEachPixel)*float(pix)) + " " + " sqr Km. [" + str(pix) + "/"+ str(totalPix) + " pix/img]"
 print pixelArea
 light_green_blur = cv2.medianBlur(light_green_res,3)    #ODD values only for median filter
 cv2.putText(img,'Original',(70,50), font, 1, (255,255,255), 2, cv2.LINE_AA)
@@ -187,6 +195,7 @@ cv2.putText(img,'Original',(70,50), font, 1, (255,255,255), 2, cv2.LINE_AA)
 cv2.putText(light_green_mask,'light_greenMask',(50,50), font, 1, (255,255,255), 2, cv2.LINE_AA)
 #cv2.imshow('mask',light_green_mask)
 cv2.putText(light_green_res,'light_greenProcessed',(50,50), font, 1, (255,255,255), 2, cv2.LINE_AA)
+cv2.putText(light_green_res, "Area: "+str(float(areaOfEachPixel)*float(pix))+" sqr Km.", (50,100),cv2.FONT_HERSHEY_SIMPLEX, 0.85, (255,255,255), 2)
 #cv2.imshow('res',light_green_res)
 cv2.putText(light_green_blur,'light_greenFiltered',(50,50), font, 1, (255,255,255), 2, cv2.LINE_AA)
 #cv2.imshow('blur',light_green_blur)
@@ -201,7 +210,7 @@ dark_green_mask = cv2.inRange(hsv, dark_green_lower, dark_green_upper)
 dark_green_res = cv2.bitwise_and(img, img, mask= dark_green_mask)
 pix = cv2.countNonZero(dark_green_mask)
 totalPix = cv2.countNonZero(grayimg);
-pixelArea = "Pixel area of dark_green: \t" + str(float(pix)/totalPix*100.0) + "% [" + str(pix) + "/"+ str(totalPix) + " pix/image]"
+pixelArea = "Pixel area of dark_green: \t" + str(float(areaOfEachPixel)*float(pix)) + " " + " sqr Km. [" + str(pix) + "/"+ str(totalPix) + " pix/img]"
 print pixelArea
 dark_green_blur = cv2.medianBlur(dark_green_res,3)    #ODD values only for median filter
 cv2.putText(img,'Original',(70,50), font, 1, (255,255,255), 2, cv2.LINE_AA)
@@ -209,6 +218,7 @@ cv2.putText(img,'Original',(70,50), font, 1, (255,255,255), 2, cv2.LINE_AA)
 cv2.putText(dark_green_mask,'dark_greenMask',(50,50), font, 1, (255,255,255), 2, cv2.LINE_AA)
 #cv2.imshow('mask',dark_green_mask)
 cv2.putText(dark_green_res,'dark_greenProcessed',(50,50), font, 1, (255,255,255), 2, cv2.LINE_AA)
+cv2.putText(dark_green_res, "Area: "+str(float(areaOfEachPixel)*float(pix))+" sqr Km.", (50,100),cv2.FONT_HERSHEY_SIMPLEX, 0.85, (255,255,255), 2)
 #cv2.imshow('res',dark_green_res)
 cv2.putText(dark_green_blur,'dark_greenFiltered',(50,50), font, 1, (255,255,255), 2, cv2.LINE_AA)
 #cv2.imshow('blur',dark_green_blur)
