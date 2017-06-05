616c5b9b70274a8db49c2b3325abb46874bc3882
Pranjal Joshi <joshi.pranjal5@gmail.com>
2017-06-05 18:17:20 +0530

    HPC files tested.

---
+++ Python/allcloud_hpc.py
@@ -0,0 +1,298 @@
+#!/usr/bin/python
+
+# Author: Pranjal Joshi
+# Date 	: 26-10-2016
+
+import os
+import sys
+import argparse
+import time
+#import MySQLdb as mdb
+import pymysql as mdb
+import matplotlib.pyplot as plt
+import numpy as np
+import multiprocessing
+import math
+import progressbar
+
+# Global varables
+THREADS = 16
+SEQUENTIAL = False
+SHOW_PLOT = False
+dirPath = os.path.abspath('.')		#"/home/cyberfox/iitm/"
+if(not(os.path.isdir(os.path.join(dirPath,'cloudTracking')))):
+	os.system("mkdir cloudTracking")
+rangeCommand = ""
+XDIVS = 10
+SMOOTHENER = 1
+p = None
+c = None
+cmds = []
+z_new = []
+BLOCK_CHAR = u"\u2588"
+
+# for printing colorful text
+class colorText:
+	HEAD = '\033[95m'
+	BLUE = '\033[94m'
+	GREEN = '\033[92m'
+	WARN = '\033[93m'
+	FAIL = '\033[91m'
+	END = '\033[0m'
+	BOLD = '\033[1m'
+	UNDR = '\033[4m'
+
+os.system("clear")
+
+# Database connection manager
+try:
+	print(colorText.GREEN + "Initializing database to store analyzed data..." + colorText.END)
+	con = mdb.connect("localhost","subrata","")
+	db = con.cursor()
+except Exception as e:
+	try:
+		con = mdb.connect("localhost","subrata","")
+		db = con.cursor()
+		db.execute("use test")
+	except Exception as e:
+		raise e
+		sys.exit(colorText.FAIL + "Failed to connect MySQL database! Check credentials & make sure that MySQL server is running in background." + colorText.END)
+
+def initDB():
+	try:
+		db.execute("show databases")
+		a = db.fetchall()
+		a = str(a)
+		if(a.find("test") > 0):
+        		#db.execute("drop database test")
+			db.execute("use test")
+			db.execute('drop table image_table')
+			db.execute('drop table cloud_table')
+			con.commit()
+		else:
+			pass
+		# Create new database while starting batch processing each time.
+		#db.execute("create database test")
+		db.execute("use test")
+		db.execute("create table image_table(iid INT AUTO_INCREMENT, datetime VARCHAR(40), PRIMARY KEY(iid))")
+		db.execute("create table cloud_table(iid INT, cid INT, area FLOAT)")
+		con.commit()
+		print(colorText.GREEN + "Database created successfully!\n" + colorText.END)
+	except Exception as e:
+		raise e
+		sys.exit(colorText.FAIL + "Something went wrong, Failed to initialize database correctly!!" + colorText.END)
+
+def closeDB():
+	print(colorText.WARN + "\nClosing database...\n" + colorText.END)
+	con.close()
+
+def smooth(y, box_pts):
+    box = np.ones(box_pts)/box_pts
+    y_smooth = np.convolve(y, box, mode='same')
+    return y_smooth
+
+def plotGraph():
+	print(colorText.BOLD + colorText.UNDR + colorText.HEAD + "\nPlotting analyzed data... Please wait...\n" + colorText.END)
+	db.execute("use test")
+	db.execute("select datetime from image_table")
+	fetchTime = db.fetchall()
+	cnt = len(fetchTime)
+	timeArray = []
+	correctionCount = 0
+	for i in range(0,cnt):
+		if((i%(round(cnt/XDIVS)+1) == 0) or (i == cnt-1)):
+			if((int(fetchTime[i][0][0]) != 1) and (int(fetchTime[i][0][0]) != 0)):		# check if time starts with 0x.min or 1x.min
+				temp = "0" + fetchTime[i][0]
+				timeArray.append(temp)
+			else:
+				temp = fetchTime[i][0]
+				timeArray.append(temp)
+			correctionCount += 1
+		else:
+			pass
+	db.execute("select max(iid) from cloud_table")
+	maxImages = db.fetchone()
+	maxImages = int(maxImages[0])
+	db.execute("select max(cid) from cloud_table")
+	maxClouds = db.fetchone()
+	maxClouds = int(maxClouds[0])
+	db.execute("select max(area) from cloud_table")
+	maxArea = db.fetchone()
+	maxArea = int(maxArea[0])
+
+	fig = plt.figure(figsize=(13.6,7.68))
+
+	yMax = []
+
+	for cldCnt in range(1,maxClouds+1):
+		db.execute("select area from cloud_table where cid=%s" % cldCnt)
+		fetchArea = db.fetchall()
+		cnt = len(fetchArea)
+		areaArray = np.zeros((maxImages),np.float16)
+		for k in range(0,cnt):
+			#db.execute("select iid from cloud_table where cast(area as decimal)=cast(%s as decimal)" % str(fetchArea[k][0]))
+			db.execute("select iid from cloud_table where abs(area - %s) < 0.0005" % str(fetchArea[k][0]))
+			index = db.fetchone()
+			index = int(index[0])
+			areaArray[index-1] = float(fetchArea[k][0])
+
+		# start plotting
+		xAxis = np.arange(0,len(fetchTime))
+		areaArray = np.array(areaArray)
+		yAxis = areaArray
+		yAxis = np.pad(yAxis, pad_width=(len(fetchTime)-len(yAxis)), mode='constant', constant_values=0)[len(fetchTime)-len(yAxis):]	# 0 pad for dimesion match
+		xTicks = timeArray
+		#print("xAxis: ", xAxis)
+		#print("yAxis: ", yAxis)
+		plt.xticks(xAxis, xTicks)
+		plt.ylim((0,maxArea+20))
+		plt.xlabel("Time",fontsize=15)
+		plt.ylabel("Area (Sq.KM)",fontsize=15)
+		plt.title("Cloud Area Statistics",fontsize=30)
+		plt.figtext(.5,.86,('For reflectivity range: %s to %s dB.' % (lowerRange, upperRange)),fontsize=10,ha='center')
+		plt.grid(True)
+		plt.locator_params(axis='x', nbins=XDIVS)		# limits x-labels to 10. Eliminates crowd on X axis.
+		plt.plot(xAxis, smooth(yAxis,SMOOTHENER), linewidth=1.5, label=("Cloud " + str(cldCnt)),marker='o') #marker='o'
+		plt.legend(loc='upper right', prop={'size':10})
+
+		if cldCnt == 1:						# to plot effective area
+			yMax = np.zeros_like(yAxis)
+		yMax = np.vstack((yMax,yAxis))
+
+	fig.savefig(dirPath + "/cloudTracking/analyzedPlot.png")
+
+	# effective area plot
+	plt.clf()
+	yMax = np.sort(yMax,axis=0)
+	yMax = yMax[1:]
+	#yMax = np.flip(yMax ,axis=0)
+	plt.gca().set_prop_cycle(None)
+	plt.xticks(xAxis, xTicks)
+	plt.ylim((0,maxArea+20))
+	plt.xlabel("Time",fontsize=15)
+	plt.ylabel("Area (Sq.KM)",fontsize=15)
+	plt.title("Effective Cloud Area Statistics",fontsize=30)
+	plt.figtext(.5,.86,('For reflectivity range: %s to %s dB.' % (lowerRange, upperRange)),fontsize=10,ha='center')
+	plt.grid(True)
+	plt.locator_params(axis='x', nbins=XDIVS)		# limits x-labels to 10. Eliminates crowd on X axis.
+	for i in range(0,yMax.shape[0]):
+		plt.plot(xAxis,smooth(yMax[i],SMOOTHENER),linewidth=1.5,label=("Cloud " + str(cldCnt)),marker='o')
+	plt.legend(loc='upper right', prop={'size':10})
+	fig.savefig(dirPath + "/cloudTracking/effectivePlot.png")
+	###
+
+	if SHOW_PLOT:
+		plt.show()
+
+def ex(d):
+	os.system(d)
+
+def rangeMap(x,inmin,inmax,outmin,outmax,roundoff=False):
+	z = (x-inmin)*(outmax-outmin)/(inmax-inmin)+outmin;
+	if not roundoff:
+		return z
+	else:
+		return round(z)
+
+def runThread():
+	global c
+	tmp = 0
+	bar = progressbar.ProgressBar(maxval=int(math.ceil(len(z_new)/THREADS)+2),widgets=[progressbar.Bar(BLOCK_CHAR,'[',']'),' ',progressbar.Percentage()])
+	print("Processing...\n")
+	bar.start()
+	for c in range(1,int(math.ceil(len(z_new)/THREADS)+2)):
+		global p
+		jobs = []
+		for i in range(THREADS):
+			try:
+				p = multiprocessing.Process(target=ex,args=(cmds[tmp+i],))
+				jobs.append(p)
+				p.start()
+				bar.update(c)
+				time.sleep(0.25)
+			except:
+				print("\nAll threads have been started.")
+				break
+		try:
+			p.join()
+		except:
+			pass
+		p = None
+		tmp += THREADS
+	bar.finish()
+
+# initialize MySQL database to store analyzed data.
+initDB()
+
+t = time.time()
+z = sorted(os.listdir(dirPath))			# Read files in sequential manner.
+l = len(z)
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-x","--diameter",required=True,help="RADAR diameter in KM [ X axis ]")
+ap.add_argument("-y","--height",required=True,help="RADAR height in KM [ Y axis ]")
+ap.add_argument("-l","--lower",required=False,help="Lower range of Reflectivity")
+ap.add_argument("-u","--upper",required=False,help="Upper range of Reflectivity")
+ap.add_argument("-f","--filter",required=False,help="Apply smoothening filter of specified window")
+args = vars(ap.parse_args())
+
+lowerRange = int(args["lower"])
+upperRange = int(args["upper"])
+
+if args["filter"] == None:
+	SMOOTHENER = 1
+else:
+	SMOOTHENER = int(args["filter"])
+
+if(lowerRange != None and upperRange != None):
+	rangeCommand = " -l " + str(lowerRange) + " -u " + str(upperRange)
+else:
+	rangeCommand = ""
+
+RADAR_DIAMETER = int(args["diameter"])
+RADAR_HEIGHT = int(args["height"])
+print(colorText.WARN + colorText.BOLD + "USER INPUT PARAMETERS:\n\nTotal distace on X-axis :%s KMs\nTotal height on Y-axis :%s KMs" % (str(RADAR_DIAMETER),str(RADAR_HEIGHT)))
+print("Upper reflectivity limit(dB): %s\nLower reflectivity limit(dB): %s\nSmoothening filter: %d\n" % (str(upperRange),str(lowerRange),SMOOTHENER) + colorText.END)
+
+print(colorText.BLUE + colorText.BOLD + "SYSTEM INFORMATION:\n\nAvailabe CPU Cores: %d" % (multiprocessing.cpu_count()))
+print("No. of parallel threads: %s\n" % (str(THREADS) if (SEQUENTIAL == False) else "1 [SEQUENTIAL flag is activated manually]") + colorText.END)
+
+if SEQUENTIAL:
+	for i in range(0,l):
+		s = str(z[i])
+		if(s.find('KASPR') > -1):
+			z_new.append(s)
+
+	l = len(z_new)
+	z = z_new
+	bar = progressbar.ProgressBar(maxval=l,widgets=[progressbar.Bar(BLOCK_CHAR,'[',']'),' ',progressbar.Percentage()])
+	print("Processing...\n")
+	bar.start()
+	for i in range(0,l):
+                s = str(z[i])
+                if (s.find('KASPR') > -1):
+                        cmd = "python3 cloud.py -i " + s + " -x " + str(RADAR_DIAMETER) + " -y " + str(RADAR_HEIGHT) + rangeCommand
+                        bar.update(i)
+                        os.system(cmd)
+                else:
+	                pass
+	bar.finish()
+else:
+	# Pre-process paths for parallel execution
+	for i in range(0,l):
+		temp = z[i]
+		if(temp.find('KASPR') == -1):
+			pass
+		else:
+			z_new.append(temp)
+
+	for i in range(0,len(z_new)):
+		cmd = "python3 ./cloud.py -i " + z_new[i] + " -x " + str(RADAR_DIAMETER) + " -y " + str(RADAR_HEIGHT) + rangeCommand
+		cmds.append(cmd)
+
+	# Multithreading begins...
+	runThread()
+
+plotGraph()
+print(colorText.GREEN + "\nTime required for script execution: "+str(round(time.time()-t,3))+" Seconds\n" + colorText.END)
+closeDB()
---
+++ Python/cloud_hpc.py
@@ -0,0 +1,430 @@
+#!/usr/bin/python
+
+# Author: Pranjal Joshi
+# Date 	: 24-10-2016
+# Script to calculate cloud area using image processing.
+
+import cv2
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+import argparse
+import os
+import sys
+import imutils
+#import MySQLdb as mdb
+import pymysql as mdb
+from multiprocessing import Process, Value
+
+# for printing colorful text
+class colorText:
+	HEAD = '\033[95m'
+	BLUE = '\033[94m'
+	GREEN = '\033[92m'
+	WARN = '\033[93m'
+	FAIL = '\033[91m'
+	END = '\033[0m'
+	BOLD = '\033[1m'
+	UNDR = '\033[4m'
+
+# Open database connection
+try:
+	con = mdb.connect("localhost","subrata","")
+	db = con.cursor()
+	db.execute("use test")
+except Exception as e:
+	try:
+		con = mdb.connect("localhost","subrata","")
+		db = con.cursor()
+		db.execute("use test")
+	except Exception as e:
+		raise e
+		sys.exit(colorText.FAIL + "Failed to connect MySQL database! Check credentials & make sure that MySQL server is running in background." + colorText.END)
+
+### CONSTANTS ###
+STEPWISE = False
+SHOW_IMAGES = False
+STORE_ORIGNINAL_IMAGE = False
+MINIMUM_CLOUD_AREA = 1660
+MULTIPROCESSING = True
+OCR = False
+path = "/home/cyberfox/iitm/c1.png"
+savePath = "/home/cyberfox/iitm/cloudTracking/"
+thisIID = 0
+
+# Reflectivity values
+COLORBAR = [-32,-28,-24,-16,-12,-8,-4,0,8,12,16,24,28,32,40]
+
+# HSV color ranges
+COLOR_HSV_ARRAY = [np.array([120,255,255]),
+					np.array([90,255,255]),
+					np.array([75,255,255]),
+					np.array([60,255,255]),
+					np.array([60,255,175]),
+					np.array([70,255,150]),
+					np.array([30,255,255]),
+					np.array([24,255,255]),
+					np.array([20,255,255]),
+					np.array([10,255,255]),
+					np.array([0,255,255]),
+					np.array([168,255,255]),
+					np.array([150,255,255]),
+					np.array([150,127,255]),
+					np.array([150,52,255])
+					]
+
+## HSV white eliminate boundaries ##				## --> Remove white backgrounds
+lower = np.array([0,52,0])
+upper = np.array([180,255,255])
+#################################################################
+
+def checkPath(path):	# check for valid path
+	if os.path.exists(path):
+		return True
+	else:
+		return False
+
+def sortContours(cnt):	# sort from left-to-right
+	reverseSort = True
+	boundingBoxes = [cv2.boundingRect(c) for c in cnt]
+	try:
+		(cnts, boundingBoxes) = zip(*sorted(zip(cnt, boundingBoxes),key=lambda b:b[1][0], reverse=reverseSort))
+	except Exception as e:
+		raise e
+	return (cnts, boundingBoxes)
+
+def closeDB():
+	con.close()
+
+def getOCR(path):
+	global thisIID
+	if OCR:				# Deprecated ---> Less accurate, More computation
+		import pytesseract
+		from PIL import Image
+		con = mdb.connect("localhost","subrata","")
+		db = con.cursor()
+		db.execute("use test")
+		img = Image.open(path)
+		txt = pytesseract.image_to_string(img,lang="eng")
+		index = txt.find('Deg. ') + 17 #5
+		datetime = txt[index:index+9] #20]
+		datetime = datetime.replace('T',' ')
+		# rectify OCR output if error occurs
+		datetime = datetime.replace('Z','2')
+		datetime = datetime.replace('O','0')
+		datetime = datetime.replace('\n','')
+		datetime = datetime.replace(' ','')
+		#print(colorText.HEAD + "Image captured at : %s" % datetime + colorText.END)
+		db.execute("insert into image_table(datetime) values ('%s')" % datetime)
+		con.commit()
+		pass
+	else:
+		con = mdb.connect("localhost","subrata","")
+		db = con.cursor()
+		db.execute("use test")
+		path = path.split('_')
+		s = path[1]
+		s = s.split('-')
+		d = s[0]
+		t = s[1]
+		time = t[0:2] + ":" + t[2:4] + ":" + t[4:6] + "\n"
+		date = d[0:4] + "-" + d[5:6] + "-" + d[7:8]
+		time = time + date
+		#print(colorText.HEAD + ("Image captured at : %s" % time) + colorText.END)
+		db.execute("insert into image_table(datetime) values ('%s')" % time)
+		con.commit()
+	db.execute("select iid from image_table order by iid desc limit 1")		# fetch iid of last row
+	thisIID = db.fetchone()
+	thisIID = int(thisIID[0])
+	#print(colorText.HEAD + "Image number: " + str(thisIID) + colorText.END)
+
+bgmask = 0
+def getPixelArea(shared_var):
+	global PIXEL_AREA
+	global bgmask
+	#background subtractor
+	bgsub = cv2.createBackgroundSubtractorMOG2()
+	bgmask = bgsub.apply(img)
+	r, bgmask = cv2.threshold(bgmask,127,255,cv2.THRESH_BINARY)
+	bgmaskImg = bgmask.copy()							## --> contour separation algorithm
+	_,bgmaskContours,_ = cv2.findContours(bgmaskImg, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+	global leftmost
+	global rightmost
+	global topmost
+	global bottommost
+
+	for bgmaskCnt in bgmaskContours:
+		global topmost
+		global rightmost
+		global leftmost
+		global bottommost
+		area = cv2.contourArea(bgmaskCnt)
+		if area > 100000:
+			leftmost = tuple(bgmaskCnt[bgmaskCnt[:,:,0].argmin()][0])
+			rightmost = tuple(bgmaskCnt[bgmaskCnt[:,:,0].argmax()][0])
+			topmost = tuple(bgmaskCnt[bgmaskCnt[:,:,1].argmin()][0])
+			bottommost = tuple(bgmaskCnt[bgmaskCnt[:,:,1].argmax()][0])
+
+
+	RADAR_START_POINT_X = bottommost[0] #253
+	RADAR_END_POINT_X = rightmost[0] #1494
+
+	PIXEL_X_SIDE = float(RADAR_DIAMETER)/(RADAR_END_POINT_X - RADAR_START_POINT_X)
+
+	RADAR_START_POINT_Y = bottommost[1] #1362
+	RADAR_END_POINT_Y = topmost[1] #153
+
+	PIXEL_Y_SIDE = float(RADAR_HEIGHT)/(RADAR_START_POINT_Y - RADAR_END_POINT_Y)
+
+	PIXEL_AREA = float(PIXEL_Y_SIDE)*PIXEL_X_SIDE
+	shared_var.value = PIXEL_AREA
+
+## GLOBAL VARIABLES ##
+PIXEL_AREA = 0
+multiprocessing_pixel_area = Value('d',0.0)
+##
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i","--image",required=True,help="Path to image")
+ap.add_argument("-x","--diameter",required=True,help="RADAR diameter in KM [ X axis ]")
+ap.add_argument("-y","--height",required=True,help="RADAR height in KM [ Y axis ]")
+ap.add_argument("-l","--lower",required=False,help="Lower range of Reflectivity")
+ap.add_argument("-u","--upper",required=False,help="Upper range of Reflectivity")
+args = vars(ap.parse_args())
+
+path = args["image"]
+RADAR_DIAMETER = int(args["diameter"])
+RADAR_HEIGHT = int(args["height"])
+lowerRange = int(args["lower"])
+upperRange = int(args["upper"])
+rangeScale = "Reflectivity range: " + str(lowerRange) + " to " + str(upperRange) + " (in dB)"
+
+#print(colorText.BLUE + "\nOpening image: ", path, colorText.END)
+
+if checkPath(path):
+    img = cv2.imread(path)
+    outputImg = img
+else:
+    sys.exit(colorText.BOLD + colorText.FAIL + "Invalid image path is given!" + colorText.FAIL + " : " + path)
+
+if MULTIPROCESSING:
+	ocrProcess = Process(target=getOCR,args=(path,))
+	pixelAreaProcess = Process(target=getPixelArea,args=(multiprocessing_pixel_area,))
+	ocrProcess.start()
+	pixelAreaProcess.start()
+else:
+	getOCR(path)				# get datetime of image by applying OCR.
+	getPixelArea(multiprocessing_pixel_area)
+
+# HSV cloud separation								## --> Convert to HSV colorspace
+hsvImg = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+mask = cv2.inRange(hsvImg, lower, upper)
+
+# erodsion											## --> Morphological erosion & dilation to remove markings plotted on the image.
+erodeMask = np.ones((5,5), np.uint8)
+erodedImg = cv2.erode(mask, erodeMask, iterations=1)
+
+# dialation
+dilateMask = np.ones((5,5), np.uint8)
+dilatedImg = cv2.dilate(erodedImg, dilateMask, iterations=1)
+
+modifiedImg = cv2.bitwise_and(img,img, mask=dilatedImg)		## --> Mask hsv colorspace with original image. cloud separated from background.
+
+########################## NEW CODE - 18 Jan
+if(lowerRange != None and upperRange != None):
+	try:
+		lowerIndex = COLORBAR.index(lowerRange)
+		upperIndex = COLORBAR.index(upperRange)
+		if(upperRange < lowerRange):
+			sys.exit(colorText.FAIL + "Upper range can't be smaller than lower range!" + colorText.END)
+	except Exception as e:
+		#print(colorText.FAIL + "Given color value is not in range!\nColor Ranges ->" + str(COLORBAR) + colorText.END)
+		try:		# Kill batch processing if running with wrong inputs.
+			os.system("sudo killall allcloud.py")
+		except:
+			pass
+		sys.exit(0)
+	### for entire image
+	loopCount = len(COLORBAR)-1
+	offsetCount = 0
+	outerMask = np.zeros((img.shape[0],img.shape[1]),np.uint8)
+	for j in range(0,loopCount+1):
+		outerMaskTemp = cv2.inRange(hsvImg, COLOR_HSV_ARRAY[offsetCount], COLOR_HSV_ARRAY[offsetCount])
+		offsetCount = offsetCount + 1
+		outerMask = outerMaskTemp + outerMask
+	outerRegion = cv2.bitwise_and(modifiedImg,modifiedImg, mask=outerMask)
+
+	### for entire image - eliminate outer part
+	loopCount = lowerIndex
+	offsetCount = 0
+	outerMask2 = np.zeros((img.shape[0],img.shape[1]),np.uint8)
+	for j in range(0,loopCount+1):
+		outerMaskTemp = cv2.inRange(hsvImg, COLOR_HSV_ARRAY[offsetCount], COLOR_HSV_ARRAY[offsetCount])
+		offsetCount = offsetCount + 1
+		outerMask2 = outerMaskTemp + outerMask2
+	outerRegion2 = cv2.bitwise_and(modifiedImg,modifiedImg, mask=outerMask2)
+
+	### for ranged region
+	loopCount = upperIndex - lowerIndex
+	offsetCount = lowerIndex
+	colorMask = np.zeros((img.shape[0],img.shape[1]),np.uint8)
+	for j in range(0,loopCount+1):
+		colorMaskTemp = cv2.inRange(hsvImg, COLOR_HSV_ARRAY[offsetCount], COLOR_HSV_ARRAY[offsetCount])
+		offsetCount = offsetCount + 1
+		colorMask = colorMask + colorMaskTemp
+	rangedImage = cv2.bitwise_and(modifiedImg,modifiedImg, mask=colorMask)
+
+	### for innermost image - inner part elimination
+	loopCount = (len(COLORBAR)-1) - upperIndex
+	offsetCount = upperIndex+1
+	innerMask = np.zeros((img.shape[0],img.shape[1]),np.uint8)
+	for j in range(0,loopCount):
+		innerMaskTemp = cv2.inRange(hsvImg, COLOR_HSV_ARRAY[offsetCount], COLOR_HSV_ARRAY[offsetCount])
+		offsetCount = offsetCount + 1
+		innerMask = innerMaskTemp + innerMask
+	innerRegion = cv2.bitwise_and(modifiedImg,modifiedImg, mask=innerMask)
+
+	requiredRegionMask = cv2.bitwise_xor(outerMask, innerMask)
+	requiredRegionMask = cv2.bitwise_xor(requiredRegionMask, outerMask2)
+	rangedNew = cv2.bitwise_and(modifiedImg, modifiedImg, mask=requiredRegionMask)
+	rangedImage = rangedNew
+
+##########################
+
+# Graysacale original image		 					## --> Convert masked image to grayscale
+grayImg = cv2.cvtColor(rangedImage,cv2.COLOR_BGR2GRAY)
+gray_blur = grayImg
+innerGray = cv2.cvtColor(innerRegion,cv2.COLOR_BGR2GRAY)
+outerGray = cv2.cvtColor(outerRegion,cv2.COLOR_BGR2GRAY)
+#gray_blur = cv2.GaussianBlur(grayImg, (3,3), 0)	## <-- Deprecated.This reduces precesion.
+
+## --> Thresholding. convert to binary image.
+thresh = cv2.adaptiveThreshold(gray_blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY_INV, 3, 1)
+innerThresh = cv2.adaptiveThreshold(innerGray, 255, cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY_INV, 3, 1)
+outerThresh = cv2.adaptiveThreshold(outerGray, 255, cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY_INV, 3, 1)
+
+contourImg = thresh.copy()							## --> contour separation algorithm
+_,contours,_ = cv2.findContours(contourImg, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+(contours, boundingBoxes) = sortContours(contours)
+
+colorBoxContour = contours[0]
+contours = contours[1:len(contours)]				## --> Exclude color bar area calculations :)
+
+#print("\nAreas of all clouds (MINIMUM_CLOUD_AREA = %d pixels):\n" % MINIMUM_CLOUD_AREA)
+contourCounter = 0
+
+for cnt in contours:
+	area = cv2.contourArea(cnt)
+	if(area > MINIMUM_CLOUD_AREA):
+		M = cv2.moments(cnt)
+		cX = int(M["m10"] / M["m00"])
+		cY = int(M["m01"] / M["m00"])
+		#### new code - 29 JAN - cloud cropping & thresholding ###
+		x,y,w,h = cv2.boundingRect(cnt)
+		cv2.rectangle(outputImg,(x,y),(x+w,y+h),(0,0,255),3)
+		croppedCloud = gray_blur[y:y+h, x:x+w]
+		ret,croppedCloudThresh = cv2.threshold(croppedCloud,1,255,cv2.THRESH_BINARY)
+		area = cv2.countNonZero(croppedCloudThresh)
+		####
+		outputImg = cv2.drawContours(img, contours, -1, (0,0,0), 1)
+		cv2.circle(outputImg, (cX, cY), 4, (0,0,0), -1)
+		if MULTIPROCESSING:
+			PIXEL_AREA = multiprocessing_pixel_area.value
+		printArea = (area*PIXEL_AREA)
+		sqlArea = float('%.6f' % printArea)
+		printArea = ('%.3f' % printArea) + " Sq.Kms "				## truncate to 3 decimals
+		contourCounter = contourCounter + 1
+		#print(printArea + "\t-> cloud number ->\t" + str(contourCounter))
+		cv2.putText(outputImg, printArea, (cX - 20, cY - 20),cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,0), 2)
+		try:
+			cv2.putText(outputImg, str(contourCounter), (x+w-10,y-20),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
+		except Exception as e:
+			pass
+		db.execute("select iid from image_table order by iid desc limit 1")		# fetch iid of last row
+		thisIID = db.fetchone()
+		try:
+			thisIID = int(thisIID[0])#+1
+		except:					# NoneType Exception
+			thisIID = 1;
+		db.execute("insert into cloud_table (iid, cid, area) values (%s, %s, %s)" % (str(thisIID), str(contourCounter), str(sqlArea)))
+		con.commit()
+	else:
+		pass
+
+contours = list(contours)
+contours.insert(0, colorBoxContour)
+contours = tuple(contours)
+outputImg = cv2.drawContours(outputImg, contours, -1,(0,0,0),2)
+
+innerCopy = innerThresh.copy()
+_,innerContours,_ = cv2.findContours(innerCopy, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+(innerContours, boundingBoxes) = sortContours(innerContours)
+innerContours = innerContours[1:len(innerContours)]
+
+for c in innerContours:
+	outputImg = cv2.drawContours(outputImg, innerContours, -1, (0,0,0), 2)
+	pass
+
+if STEPWISE:
+	k = cv2.imread(path)
+	cv2.putText(k,'Source Image',(50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2, cv2.LINE_AA)
+	if STORE_ORIGNINAL_IMAGE:
+		storePath = savePath + path + 'source.png'
+		cv2.imwrite(storePath,k)
+
+	cv2.putText(modifiedImg,'Background elimination',(50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+	storePath = savePath + path + 'noBackground.png'
+	cv2.imwrite(storePath,modifiedImg)
+
+	cv2.putText(gray_blur,'Grayscaled',(50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+	storePath = savePath + path + 'grayscale.png'
+	cv2.imwrite(storePath,gray_blur)
+
+	cv2.putText(thresh,'Adaptive thresholding',(50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+	storePath = savePath + path + 'thresholding.png'
+	cv2.imwrite(storePath,thresh)
+
+	cv2.putText(bgmask,'Grid subtraction',(50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+	storePath = savePath + path + 'grid.png'
+	cv2.imwrite(storePath,bgmask)
+
+	cv2.putText(outputImg,'Cloud Area',(75,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2, cv2.LINE_AA)
+	tipText = "Scale: 1 pixel = " + str(PIXEL_AREA) + " Sq.Kms"
+	cv2.putText(outputImg,tipText,(75,80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1, cv2.LINE_AA)
+	cv2.putText(outputImg,rangeScale,(75,100), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1, cv2.LINE_AA)
+	storePath = savePath + path + 'output.png'
+	cv2.imwrite(storePath,outputImg)
+
+	if SHOW_IMAGES:
+		cv2.imshow("Source Image",k)
+		cv2.imshow("Background subtractor",bgmask)
+		cv2.imshow("Background elimination",modifiedImg)
+		cv2.imshow("Grayscale",gray_blur)
+		cv2.imshow("Adaptive thresholding",thresh)
+		cv2.imshow("Cloud Area",outputImg)
+		cv2.imshow("Color Range image",rangedImage)
+		cv2.imshow("InnerMask",innerRegion)
+		cv2.imshow("OuterRegion",outerRegion)
+		cv2.imshow("Color RangeNew image",rangedNew)
+		cv2.imshow("Cropped cloud",croppedCloud)
+		cv2.imshow("croppedThresh",croppedCloudThresh)
+
+else:
+	cv2.putText(outputImg,'Cloud Area',(75,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2, cv2.LINE_AA)
+	tipText = "Scale: 1 pixel = " + str(PIXEL_AREA) + " Sq.Kms"
+	cv2.putText(outputImg,tipText,(75,80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1, cv2.LINE_AA)
+	cv2.putText(outputImg,rangeScale,(75,100), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1, cv2.LINE_AA)
+	storePath = savePath + path + 'output.png'
+	cv2.imwrite(storePath,outputImg)
+
+if SHOW_IMAGES:
+	while True:
+	    key = cv2.waitKey(1) & 0xFF
+	    if key == ord("q"):                 ## exit loop on pressing Q key
+	        #print("\nQuitting...")
+	        break
+
+	cv2.destroyAllWindows()
+
+closeDB()
